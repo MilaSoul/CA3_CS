9f6483b9bf6e62f8ee9032bebdb67647871a588f
snelbops <mila.dig@yahoo.com>
2021-05-10 11:32:31 +0100

    Add JS

--- JavaScript.js
+++ JavaScript.js
@@ -15,4 +15,47 @@ muImput.onblur = function(){
     document.getElementById("error").style.display = "none"
 }
 
+// typing into password feald
+myInput.onkeyup = function(){
+    var LowerCaseLett = /[a-z]/g;
+    
+    if(muInput.value.match(LowerCaseLett)){
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid"); 
+    }
+// Uppercase Letter
+    var UpperCaseLett = /[A-Z]/g;
+
+    if(myInput.value.match(UpperCaseLett)){
+        uppercase.classList.remove("invalid");
+        uppercase.classList.add("valid");
+    } else {
+        uppercase.classList.remove("valid");
+        uppercase.classList.add("invalid");
+    }
+
+// Number 
+     var Num = /[0-9]/g;
+
+     if(myInput.value.match(Num)){
+         number.classList.remove("invalid");
+         number.classList.add("valid");
+     } else {
+        number.classList.remove("valid");
+         number.classList.add("invalid"); 
+     }
+
+// Pssword lenght
+     if(muInput.value.lenght >= 8) {
+        lenght.classList.remove("invalid");
+        lenght.classList.add("valid");
+    } else {
+       lenght.classList.remove("valid");
+        lenght.classList.add("invalid"); 
+    }
+
+}
 
