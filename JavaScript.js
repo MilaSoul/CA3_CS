39a0537718e6b9744e345c53a28472b1fef311d1
snelbops <mila.digyahoo.com>
2021-05-13 08:39:39 +0100

    js total price

--- JavaScript.js
+++ JavaScript.js
@@ -4,17 +4,20 @@ var myInput = document.getElementById("pass");
 var letter = document.getElementById("letter");
 var uppercase = document.getElementById("uppercase");
 var number = document.getElementById("number");
-var lenght = document.getElementById("lenghtpass");
+var lengthpass = document.getElementById("lengthpass");
 
-//showing the error box  
+
+///showing the error box  
 myInput.onfocus = function(){
     document.getElementById("error").style.display = "block"
 }
+
 
 //remove the eeror box from the screen
 myInput.onblur = function(){
     document.getElementById("error").style.display = "none"
 }
+
 
 // typing into password feald
 myInput.onkeyup = function(){
@@ -50,12 +53,12 @@ myInput.onkeyup = function(){
      }
 
 // Pssword lenght
-     if(myInput.value.lenght >= 8) {
-        lenght.classList.remove("invalid");
-        lenght.classList.add("valid");
+     if(myInput.value.length >= 8) {
+        lengthpass.classList.remove("invalid");
+        lengthpass.classList.add("valid");
     } else {
-       lenghtpass.classList.remove("valid");
-        lenghtpass.classList.add("invalid"); 
+       lengthpass.classList.remove("valid");
+        lengthpass.classList.add("invalid"); 
     }
 
 }
@@ -94,22 +97,66 @@ document.getElementById('output').innerHTML = output;
 
 var total_items = 12;
 
-        function CalculateItemsValue(){
-            var total = 0;
 
-            for (let i = 1; i <= total_items; i++) {
-               itemID =document.getElementById("q_" + i);
+        function calculateItemsValue() {
+            var total = 0;
+            var total_Starters = 0;
+            var total_Main = 0;
+            var total_Deserts
+
+           /*for (let i = 1; i <= total_items; i++) {
+              itemID =document.getElementById("q_" + i);
+             // itemID =document.querySelectorAll("#q_1, #q_2, #q_3" + i);
                total = total + parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price"));
                 
-            }
+            }*/
+            document.querySelectorAll('[id^="q_"]').forEach(item => {
+                var item_price = parseInt(item.value)*parseInt(item.getAttribute("data-price"))
+                var item_cat = item.getAttribute("data-cat");
+                if (!isNaN(item_price)) {
+                    if (item_cat == 'Starters') {
+                        total_Starters += item_price;
+                    }
+                   if (item_cat == 'Main') {
+                    total_Main += item_price;
+                    }  
+                    if (item_cat == 'Deserts'){
+                        total_Dserts == +- item_price;
+                    }
+
+                }
+                 
+                total = total + item_price;
+                });
+
             document.getElementById('ItemsTotal').innerHTML = "$" + total;
+            document.getElementById('ItemTotalStarters').innerHTML = "$" + total_Starters;
+            document.getElementById('ItemTotalMain').innerHTML = "$" + total_Main;
+            document.getElementById('ItemTotalDeserts').innerHTML = "$" + total_Deserts;
         }
 
         document.querySelectorAll('[id^="q_"]').forEach(item => {
-            item.addEventListener('keyup', CalculateItemsValue);
+            item.addEventListener('keyup', calculateItemsValue);
         });
 
-var starters = 3;
+      
 
 
+
+
+       
+      
+function CalculateStartersValue(){
+
+    var total_s = 0;
+    for (let n = 1; n <= total_starters; n++) {
+        startersId = document.querySelectorAll("#q_1,#q_2,#q_2," + n);
+        total_s = total_s + parselInt(startersId.value) * parselInt(startersId.getAttribute("data-price"));
+    }
+    document.getElementById('ItemsTotalStarters').innerHTML = "$" + total_s;
+
+    document.querySelectorAll('[id^="q_1"]').forEach(item_s => {
+        item_s.addEventListener('keyup', CalculateStartersValue);
+    });
+}
 
